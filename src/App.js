1b493fc5a7f87a39e996e54ce52dcd712df36c8a
Chrugez <thanhcuis99@gmail.com>
2023-08-08 22:22:02 +0700

    #5 Coding slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
-import { Home, Login, Public } from './containers/public';
-import { ToastContainer, toast } from 'react-toastify';
+import { Home, Login, Public, Personal } from './containers/public';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Routes, Route } from 'react-router-dom'
 import path from './ultis/path';
+import { useEffect } from 'react';
+import * as actions from './store/actions'
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(actions.getHome())
+  }, [])
 
   return (
     <>
@@ -14,6 +20,7 @@ function App() {
           <Route path={path.PUBLIC} element={<Public />}>
             <Route path={path.HOME} element={<Home />} />
             <Route path={path.LOGIN} element={<Login />} />
+            <Route path={path.MY_MUSIC} element={<Personal />} />
 
             <Route path={path.STAR} element={<Home />} />
           </Route>

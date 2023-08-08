2c516d59f7d2dd71406e1c2175a0fe1865bf531c
Chrugez <thanhcuis99@gmail.com>
2023-08-08 14:07:32 +0700

    #4 Code layout header

--- src/ultis/icons.js
+++ src/ultis/icons.js
@@ -1,7 +1,18 @@
 import { MdOutlineLibraryMusic } from 'react-icons/md'
+import { PiChartLine } from 'react-icons/pi'
+import { TbChartArcs } from 'react-icons/tb'
+import { MdOutlineFeed } from 'react-icons/md'
+import { HiArrowNarrowLeft, HiArrowNarrowRight } from 'react-icons/hi'
+import { FiSearch } from 'react-icons/fi'
 
 const icons = {
-    MdOutlineLibraryMusic
+    MdOutlineLibraryMusic,
+    PiChartLine,
+    TbChartArcs,
+    MdOutlineFeed,
+    HiArrowNarrowLeft,
+    HiArrowNarrowRight,
+    FiSearch
 }
 
 export default icons
--- src/ultis/menu.js
+++ src/ultis/menu.js
@@ -1,6 +1,6 @@
 import icons from "./icons"
 
-const { MdOutlineLibraryMusic } = icons
+const { MdOutlineLibraryMusic, MdOutlineFeed, PiChartLine, TbChartArcs } = icons
 
 export const sidebarMenu = [
     {
@@ -12,16 +12,16 @@ export const sidebarMenu = [
         path: '',
         text: 'Khám phá',
         end: true,
-        icons: <MdOutlineLibraryMusic size={24} />
+        icons: <TbChartArcs size={24} />
     },
     {
         path: 'zing-chart',
         text: '#zingchart',
-        icons: <MdOutlineLibraryMusic size={24} />
+        icons: <PiChartLine size={24} />
     },
     {
         path: 'follow',
         text: 'Theo dõi',
-        icons: <MdOutlineLibraryMusic size={24} />
+        icons: <MdOutlineFeed size={24} />
     },
 ]
